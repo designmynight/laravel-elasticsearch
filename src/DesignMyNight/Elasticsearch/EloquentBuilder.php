cc4538acc32d9e98c6422fae95f8a0dd4415d6dc
Rob Taylor <rob@designmynight.com>
2018-01-29 16:06:32 +0000

    fix: querying by model type

--- src/DesignMyNight/Elasticsearch/EloquentBuilder.php
+++ src/DesignMyNight/Elasticsearch/EloquentBuilder.php
@@ -25,7 +25,7 @@ class EloquentBuilder extends BaseBuilder
 
         $this->query->from($model->getSearchIndex());
 
-        $this->query->type($model->getSearchType());
+        $this->query->whereType($model->getSearchType());
 
         return $this;
     }
