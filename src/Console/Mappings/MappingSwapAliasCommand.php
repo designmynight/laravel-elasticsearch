5b16980f31cdf8b4934521e21c3ca7340525bbe0
Jaspar Gupta <jaspar@designmynight.com>
2018-05-16 11:13:55 +0100

    fix: set guzzle client and elasticsearch host

--- src/Console/Mappings/MappingSwapAliasCommand.php
+++ src/Console/Mappings/MappingSwapAliasCommand.php
@@ -3,6 +3,7 @@
 namespace DesignMyNight\Elasticsearch\Console\Mappings;
 
 use DesignMyNight\Elasticsearch\Console\Mappings\Traits\UpdatesAlias;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 
 /**
@@ -15,11 +16,30 @@ class MappingSwapAliasCommand extends Command
 
     use UpdatesAlias;
 
+    /** @var Client $client */
+    protected $client;
+
     /** @var string $description */
     protected $description = 'Swap Elasticsearch alias';
 
+    /** @var string $host */
+    protected $host;
+
     /** @var string $signature */
     protected $signature = 'index:swap {alias : Name of alias to be updated.} {index : Name of index to be updated to.} {old_index? : Name of current index.}';
+
+    /**
+     * MappingMigrateCommand constructor.
+     *
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        parent::__construct();
+
+        $this->client = $client;
+        $this->host = config('database.elasticsearch.host');
+    }
 
     /**
      * Execute the console command.
