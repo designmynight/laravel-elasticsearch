fdc11244b6031b3e88c45166175390d289047685
Jaspar Gupta <jaspar@designmynight.com>
2018-05-17 15:10:44 +0100

    feat: database merge config

---
+++ src/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'connections' => [
+        'driver'   => 'elasticsearch',
+        'host'     => 'localhost',
+        'port'     => 9200,
+        'database' => 'your_es_index',
+        // 'username' => 'optional_es_username',
+        // 'password' => 'optional_es_username',
+        // 'suffix'   => 'optional_es_index_suffix',
+    ]
+];
