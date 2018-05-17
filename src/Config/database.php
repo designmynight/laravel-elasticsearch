f21303bf343c0d606e90cb16d06fb04c672029c6
Jaspar Gupta <jaspar@designmynight.com>
2018-05-17 15:25:09 +0100

    refactor: uncomment and remove defaults

--- src/Config/database.php
+++ src/Config/database.php
@@ -6,10 +6,10 @@ return [
             'driver'   => 'elasticsearch',
             'host'     => env('ELASTICSEARCH_HOST', 'localhost'),
             'port'     => env('ELASTICSEARCH_PORT', 9200),
-            'database' => env('ELASTICSEARCH_DATABASE', 'your_es_index'),
-            // 'username' => env('ELASTICSEARCH_USERNAME', 'optional_es_username'),
-            // 'password' => env('ELASTICSEARCH_PASSWORD', 'optional_es_username'),
-            // 'suffix'   => env('ELASTICSEARCH_INDEX_SUFFIX', 'optional_es_index_suffix'),
+            'database' => env('ELASTICSEARCH_DATABASE'),
+            'username' => env('ELASTICSEARCH_USERNAME'),
+            'password' => env('ELASTICSEARCH_PASSWORD'),
+            'suffix'   => env('ELASTICSEARCH_INDEX_SUFFIX'),
         ]
     ]
 ];
