4e926ba999fd2fcc59d076e81b3a9319ffb4102f
Jaspar Gupta <jaspar@designmynight.com>
2018-07-04 19:04:22 +0100

    fix: client builder not initiated

--- src/Console/Mappings/IndexSwapCommand.php
+++ src/Console/Mappings/IndexSwapCommand.php
@@ -3,6 +3,7 @@
 namespace DesignMyNight\Elasticsearch\Console\Mappings;
 
 use DesignMyNight\Elasticsearch\Console\Mappings\Traits\UpdatesAlias;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Console\Command;
 
 /**
@@ -19,7 +20,22 @@ class IndexSwapCommand extends Command
     protected $description = 'Swap Elasticsearch alias';
 
     /** @var string $signature */
-    protected $signature = 'index:swap {alias : Name of alias to be updated.} {index : Name of index to be updated to.} {old-index? : Name of current index.} {--R|remove-old-index : Deletes the old index.}';
+    protected $signature = 'index:swap {alias : Name of alias to be updated.} {index? : Name of index to be updated to.} {old-index? : Name of current index.} {--R|remove-old-index : Deletes the old index.}';
+
+    /** @var ClientBuilder $client */
+    private $client;
+
+    /**
+     * IndexSwapCommand constructor.
+     *
+     * @param ClientBuilder $client
+     */
+    public function __construct(ClientBuilder $client)
+    {
+        parent::__construct();
+
+        $this->client = $client;
+    }
 
     /**
      * Execute the console command.
