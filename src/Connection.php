designmynight/laravel-elasticsearch

commit 44fce93101ae66da7724f07c3b9ae520092f0ee3

fix: use deleteByQuery to allow multiple deletes

src/Connection.php
@@ -165,7 +165,7 @@ class Connection extends BaseConnection
         return $this->run(
             $query,
             $bindings,
-            Closure::fromCallable([$this->connection, 'delete'])
+            Closure::fromCallable([$this->connection, 'deleteByQuery'])
         );
     }
 

src/QueryBuilder.php
@@ -759,7 +759,7 @@ class QueryBuilder extends BaseBuilder
 
         $result = $this->connection->delete($this->grammar->compileDelete($this));
 
-        return !empty($result['found']);
+        return !empty($result['deleted']);
     }
 
     public function __call($method, $parameters)

src/QueryGrammar.php
@@ -1133,20 +1133,7 @@ class QueryGrammar extends BaseGrammar
      */
     public function compileDelete(Builder $builder): array
     {
-        $params = [
-            'index' => $builder->from . $this->indexSuffix,
-            'id'    => (string) $builder->wheres[0]['value']
-        ];
-
-        if ($routing = $builder->getRouting()) {
-            $params['routing'] = $routing;
-        }
-
-        if ($parentId = $builder->getParentId()) {
-            $params['parent'] = $parentId;
-        }
-
-        return $params;
+        return $this->compileSelect($builder);
     }
 
     /**

commit 9e3f709f2954a59aa4799ce60f22c43d1283f1df

Fix delete operation to use correct Elasticsearch client method
The Connection class was always calling deleteByQuery for delete
operations, but when deleting a single document by ID, the
Elasticsearch client's delete method should be used instead.

This fix checks if the query has a 'body' with a 'query' to determine:
- If yes: use deleteByQuery (for deleting multiple documents based on query)
- If no: use delete (for single document deletion by ID)

This resolves the error where single document deletion fails with:
"id" is not a valid parameter. Allowed parameters are "_source",
"_source_exclude", "_source_include", ...

The issue was introduced in commit 44fce93 which changed the delete
implementation to use deleteByQuery to support multiple deletes, but
the QueryGrammar.compileDelete was later reverted back to single
document format, creating a mismatch.

Related: commit 44fce93 "fix: use deleteByQuery to allow multiple deletes"

src/Connection.php
@@ -182,10 +182,14 @@ class Connection extends BaseConnection
      */
     public function delete($query, $bindings = [])
     {
+        // If the query has a 'body' with a 'query', use deleteByQuery
+        // Otherwise, use delete for single document deletion
+        $method = isset($query['body']['query']) ? 'deleteByQuery' : 'delete';
+
         return $this->run(
             $query,
             $bindings,
-            Closure::fromCallable([$this->connection, 'deleteByQuery'])
+            Closure::fromCallable([$this->connection, $method])
         );
     }
 